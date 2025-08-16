16cdec083d272e653fe44750e05c246ebf05aa1b
Arun-Kumar-S-debug <s.arunkumar7305@gmail.com>
2025-08-16 12:04:28 +0530

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+class node{
+	String data=null;
+	node next=null;
+}
+
+class queue_operation{
+	node temp=null;
+	void push(String x){
+		node stack=new node();
+		stack.data=x;
+		stack.next=temp;
+		temp=stack;
+	}
+	void queue_push(){
+		node rev_temp=null;
+		rev_temp=temp;
+		temp=null;
+		node v=rev_temp;
+		while (v!=null){
+			push(v.data);
+			v=v.next;
+		}
+	}
+	void display(){
+		System.out.println("The queue elements are given below,");
+		node v=temp;
+		while(v!=null){
+			System.out.println(v.data);
+			v=v.next;
+		}
+	}
+	void peek(){
+		System.out.println("The peeked element is "+temp.data);
+	}
+	void Size(){
+		node v=temp;
+		int count=0;
+		while(v!=null){
+			v=v.next;
+			count++;
+		}
+		System.out.println("The number of elements im a queue is "+count);
+	}
+	void isEmpty(){
+		if (temp.data==null){
+			System.out.println("The queue is empty.");
+		}else{
+			System.out.println("The queue is not empty.");
+		}
+	}
+	void pop(){
+		if (temp.data!=null){
+			temp=temp.next;
+			System.out.println("The element is poped.");
+		}
+	}
+}
+
+class queue{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		queue_operation obj1=new queue_operation();
+		String text="""
+				Enter the given operation number to perform particular task,
+					1=push()
+					2=display()
+					3=peek()
+					4=size()
+					5=isEmpty()
+					6=pop()
+					7=reverse()
+					8=end
+						""";
+		System.out.println(text);
+		boolean j=true;
+		while (j==true){
+			System.out.println("Enter the operation number you want to perform:");
+			int operation_no=input.nextInt();
+			input.nextLine();
+			if (operation_no==1){
+				boolean i=true;
+				while(i==true){
+					System.out.println("Enter the value to insert into stack (or) end to end the operation:");
+					String val=input.nextLine();
+					if (val.equalsIgnoreCase("end")){
+						i=false;
+					}else {
+						obj1.push(val);
+					}
+				}
+				obj1.queue_push();
+			}
+			else if (operation_no==2){
+				obj1.display(); 
+			}
+			else if (operation_no==3){
+				obj1.peek();
+			}
+			else if (operation_no==4){
+				obj1.Size();
+			}
+			else if (operation_no==5){
+				obj1.isEmpty();
+			}
+			else if (operation_no==6){
+				obj1.pop();
+			}
+			else if(operation_no==7){
+				obj1.queue_push();
+			}
+			else if(operation_no==8){
+					j=false;
+			}
+			else{
+				System.out.println("Enter the correct operation number");
+			}
+		}
+	}
+}
